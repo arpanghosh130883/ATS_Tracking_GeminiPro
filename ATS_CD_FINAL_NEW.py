205f27689715f74b49d89f7425e51bd95bb0df6d
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-03-05 07:39:27 +0530

    Update ATS_CD_FINAL_NEW.py

--- ATS_CD_FINAL_NEW.py
+++ ATS_CD_FINAL_NEW.py
@@ -5,6 +5,8 @@ import streamlit as st
 import google.generativeai as genai
 import PyPDF2 as pdf
 import re
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Define the prompt template
 prompt_template = """
